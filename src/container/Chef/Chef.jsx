5d519772c74e476ba9f10206dc2ea416be9bd6ed
Henri Bresteau <henri.bresteau@gamil.com>
2022-02-24 18:56:35 +0100

    section Chef

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
-
-import './Chef.css';
+import React from "react";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className="headtext__cormorant">What we belive in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+            eum quos! Quas, at quidem. Eveniet atque totam at necessitatibus
+            illum cupiditate similique eius corporis, explicabo deserunt amet
+            suscipit. In dignissimos aliquam similique. Omnis, mollitia nulla!
+          </p>
+        </div>
+        <div className="p__opensans">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam
+          excepturi natus officiis porro explicabo nulla.
+        </div>
+        <div className="app__chef-sign">
+          <p>Kevin Luo</p>
+          <p className="p__opensans">Chef & founder</p>
+          <img src={images.sign} alt="sign" />
+        </div>
+      </div>
+    </div>
   </div>
 );
 
